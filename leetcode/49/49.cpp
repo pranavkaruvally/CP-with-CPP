b1f670d7925f3ac2a4904b3a4c644f5c120b174b
Pranav P <pranavsdreams@gmail.com>
2023-08-07 23:37:04 +0530

    Added 49

---
+++ leetcode/49/49.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string hash(string s) {
+        string hash;
+        int i=0;
+        for (int j=0; j<26; j++)
+            hash.push_back('0');
+        for (char i: s)
+            hash[i-'a']++;
+        return hash;
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
+        unordered_map<string, vector<string>> groups;
+        unordered_set<string> groupIndices;
+
+        for (string i: strs) {
+            string thisHash = hash(i);
+            groups[thisHash].push_back(i);
+            groupIndices.insert(thisHash);
+        }
+
+        for (string index: groupIndices) {
+            result.push_back(groups[index]);
+        }
+
+        return result;
+    }
+};
