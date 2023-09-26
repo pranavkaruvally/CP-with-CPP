3f729c7b7fcc99cf1358beac6bf0c0a3670ed6bc
Pranav P <pranavsdreams@gmail.com>
2023-09-26 06:55:18 +0530

    Added solution for 268 without bit manipulation

---
+++ leetcode/268/268_withoutbitmanipulation.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int res = 0;
+        for (int i=0; i<n; i++) res += i - nums[i];
+
+        return res + n;
+    }
+};
