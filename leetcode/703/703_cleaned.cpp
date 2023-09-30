0207ff8b3ff8a8f8058a86022a72e831d4de5843
Pranav P <pranavsdreams@gmail.com>
2023-09-30 12:30:17 +0530

    Cleaned 703 code

---
+++ leetcode/703/703_cleaned.cpp
@@ -0,0 +1,27 @@
+class KthLargest {
+private:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int kth;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        pq = priority_queue<int, vector<int>, greater<int>>(nums.begin(), nums.end());
+        while (pq.size() > k)
+            pq.pop();
+        kth = k;
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if (pq.size() > kth) {
+            pq.pop();
+        }
+
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
